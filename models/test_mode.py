d415bce379c706d01ea278adefe47c06dfe98728
Ovi Tisler <ovi.tisler@gmail.com>
2026-09-23 18:37:06 -0500

    Update test_mode.py

--- models/test_mode.py
+++ models/test_mode.py
@@ -3,7 +3,7 @@
 _simulate_rate_limit = False
 
 # Set to a 'YYYY-MM-DD' string to override today's date, or None to use real today
-DATE_OVERRIDE = None
+DATE_OVERRIDE = '2026-09-09'
 
 def set_simulate_rate_limit(enabled):
     """Enable/disable simulated rate limit errors for testing"""
